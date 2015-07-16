6d769ca5bda9fe488eb330648633736908b8e2a4
antho31 <anthonygourraud@gmail.com>
2015-07-16 14:16:26 +0200

    doc sentry.rb

--- lib/logstash/outputs/sentry.rb
+++ lib/logstash/outputs/sentry.rb
@@ -3,19 +3,72 @@ require 'logstash/outputs/base'
 require 'logstash/namespace'
 require 'json'
 
+# Sentry is a modern error logging and aggregation platform.
+# * https://getsentry.com/
+#
+# It’s important to note that Sentry should not be thought of as a log stream, but as an aggregator. 
+# It fits somewhere in-between a simple metrics solution (such as Graphite) and a full-on log stream aggregator (like Logstash).
+#
+# Generate and inform your client key (Settings -> Client key)
+# The client key has this form  * https://[key]:[secret]@[host]/[project_id] *
+#
+# More informations : 
+# * https://sentry.readthedocs.org/en/latest/
+
+
 class LogStash::Outputs::Sentry < LogStash::Outputs::Base
  
   config_name 'sentry'
-   
+  
+  
+  # The key of the client key 
   config :key, :validate => :string, :required => true
+  
+  # The secret  of the client key
   config :secret, :validate => :string, :required => true
+  
+  # The project id of the client key
   config :project_id, :validate => :string, :required => true
+  
+  # The Sentry host 
   config :host, :validate => :string, :default => "https://app.getsentry.com", :required => false 
+  
+  # This sets the message value in Sentry (the title of your event)
   config :msg, :validate => :string, :default => "Message from logstash", :required => false
+  
+  # This sets the level value in Sentry (the level tag)
   config :level_tag, :validate => :string, :default => "error", :required => false
+  
+  # Is the protocole https ? By default yes (host is "https://app.getsentry.com") 
+  # If you have installed Sentry in your own machine, maybe you do use http, 
+  # so you have to disable ssl ( "use_ssl" => false ) 
   config :use_ssl, :validate => :boolean, :default => true, :required => false 
+  
+  # If set to true automatically map all logstash defined fields to Sentry extra fields.
+  # As an example, the logstash event:
+  # [source,ruby]
+  #    {
+  #      "@timestamp":"2013-12-10T14:36:26.151+0000",
+  #      "@version": 1,
+  #      "message":"log message",
+  #      "host": "host.domain.com",
+  #      "nested_field": {
+  #                        "key": "value"
+  #                      }
+  #    }
+  # Is mapped to this Sentry  event:
+  # [source,ruby]
+  # extra {
+  #      "@timestamp":"2013-12-10T14:36:26.151+0000",
+  #      "@version": 1,
+  #      "message":"log message",
+  #      "host": "host.domain.com",
+  #      "nested_field": {
+  #                        "key": "value"
+  #                      }
+  #    }   
   config :fields_to_tags, :validate => :boolean, :default => false, :required => false
-
+ 
   public
   def register
     require 'net/https'
